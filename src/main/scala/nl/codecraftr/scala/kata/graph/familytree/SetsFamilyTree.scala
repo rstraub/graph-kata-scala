fd0b84796925aba1799a5d72b3463adc91e56d5a
Roy Straub <rstraub@xebia.com>
2024-07-23 11:26:11 +0200

    Siblings using sets

--- src/main/scala/nl/codecraftr/scala/kata/graph/familytree/SetsFamilyTree.scala
+++ src/main/scala/nl/codecraftr/scala/kata/graph/familytree/SetsFamilyTree.scala
@@ -1,21 +1,23 @@
 package nl.codecraftr.scala.kata.graph.familytree
 
-import scala.annotation.nowarn
-
-@nowarn
 class SetsFamilyTree(persons: Set[Person], relations: Set[Relation])
     extends FamilyTree {
-    override def members: Set[Person] = persons
+  override def members: Set[Person] = persons
 
-    override def siblingsOf(person: Person): Set[Person] = ???
+  override def siblingsOf(person: Person): Set[Person] =
+    relations
+      .collect {
+        case s: Sibling if s.from == person || s.to == person => s
+      }
+      .map(_.without(person))
 
-    override def parentsOf(person: Person): Set[Person] = ???
+  override def parentsOf(person: Person): Set[Person] = ???
 
-    override def ancestorsOf(person: Person): Set[Person] = ???
+  override def ancestorsOf(person: Person): Set[Person] = ???
 
-    override def partnerOf(person: Person): Option[Person] = ???
+  override def partnerOf(person: Person): Option[Person] = ???
 
-    override def longestMarriage: Marriage = ???
+  override def longestMarriage: Marriage = ???
 
-    override def relationBetween(from: Person, to: Person): List[String] = ???
+  override def relationBetween(from: Person, to: Person): List[String] = ???
 }
